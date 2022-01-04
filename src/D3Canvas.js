fb1e865c8000e2439d78e3d44c6b31b4e549acc1
Marvling <sinan.ozer7@protonmail.com>
2022-01-04 00:16:07 -0800

    setting up parser

--- src/D3Canvas.js
+++ src/D3Canvas.js
@@ -3,16 +3,7 @@ import * as d3 from 'd3';
 
 function D3Canvas() {
     
-    const temperatureData = [ 8, 5, 13, 9, 12 ]
-
-    d3.select(this.refs.temperatures)
-        .selectAll("h2")
-        .data(temperatureData)
-        .enter()
-            .append("h2")
-            .text("New Temperature")
-
-    return (null)
+    return (<h1>Bokum</h1> )
 }
 
 export default D3Canvas
