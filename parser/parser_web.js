99cb91846f0337114f7d810ed07fa51b4618bfe0
Marvling <sinan.ozer7@gmail.com>
2022-01-05 23:40:33 -0800

    finished parser for local data

--- parser/parser_web.js
+++ parser/parser_web.js
@@ -1,8 +1,48 @@
 
-let listNames = ['Fight Club', 'The Fog', 'Harry Potter'];
-let listData = [];
+async function dataParser() {
+
+    let url = './watched.csv';
+
+    try {
+        const response = await fetch(url);
+        const data = await response.text();
+
+        // Putting each line in an array
+        const table = data.split('\n');
+
+        // Remmoving a special character after each line (Cleaning)
+        const rows = [];
+        for (let i = 0; i < table.length; i++) {
+            const row = table[i].split('\r').join('');
+            rows.push(row.split(','));
+        };
+
+        // Making each row an object
+        const objects = [];
+        rows.forEach(e => {
+            const dateWatched = e[0];
+            const title = e[1];
+            const isWatchedInTheater = e[2];
+            const isWatchedAlone = e[3];
+
+            dataObject = { dateWatched: '', title: '', isWatchedInTheater: '', isWatchedAlone: '' };
+
+            dataObject.dateWatched = dateWatched;
+            dataObject.title = title;
+            dataObject.isWatchedInTheater = isWatchedInTheater;
+            dataObject.isWatchedAlone = isWatchedAlone;
+
+            objects.push(dataObject);
+        });
+
+        console.log(objects);
+
+    } catch (err) {
+        console.error(`this is my error!: ${err}`);
+    }
+}
    
-const getMovies = async (e) => {
+const tmdbSearch = async (e) => {
 
     let url = `https://api.themoviedb.org/3/search/movie?api_key=00decbdccac0d50538a8bdbf8085ce4a&language=en-US&query=${e}&page=1&include_adult=false`;
 
@@ -21,9 +61,12 @@ const getMovies = async (e) => {
     console.log(listData)
 }
 
-for (let i = 0; i < listNames.length; i++) {
-    const e = listNames[i];
+function getJson (movieNames) {
 
-    getMovies(e)
-    
+    for (let i = 0; i < movieNames.length; i++) {
+        const e = listNames[i];
+        tmdbSearch(e);
+    }
 }
+
+dataParser();
